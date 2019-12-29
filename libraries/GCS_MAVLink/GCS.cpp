0b0f25c27cbb3b7133016e1aebe3e1b5cb6a0e95
Peter Barker <pbarker@barker.dropbear.id.au>
2019-12-29 20:37:51 +1100

    GCS_MAVLink: have gps is_healthy return a failmsg

--- libraries/GCS_MAVLink/GCS.cpp
+++ libraries/GCS_MAVLink/GCS.cpp
@@ -145,7 +145,7 @@ void GCS::update_sensor_status_flags()
         control_sensors_present |= MAV_SYS_STATUS_SENSOR_GPS;
         control_sensors_enabled |= MAV_SYS_STATUS_SENSOR_GPS;
     }
-    if (gps.is_healthy() && gps.status() >= min_status_for_gps_healthy()) {
+    if (gps.is_healthy(nullptr, 0) && gps.status() >= min_status_for_gps_healthy()) {
         control_sensors_health |= MAV_SYS_STATUS_SENSOR_GPS;
     }
 
