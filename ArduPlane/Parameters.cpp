d8d5322e99241cd7b84d16f4bfedeee7475704e9
Peter Barker <pbarker@barker.dropbear.id.au>
2020-04-03 18:14:47 +1100

    ArduPlane: allow registration of validation functions for parameter values

--- ArduPlane/Parameters.cpp
+++ ArduPlane/Parameters.cpp
@@ -8,6 +8,7 @@
 #define GSCALAR(v, name, def) { plane.g.v.vtype, name, Parameters::k_param_ ## v, &plane.g.v, {def_value : def} }
 #define ASCALAR(v, name, def) { plane.aparm.v.vtype, name, Parameters::k_param_ ## v, (const void *)&plane.aparm.v, {def_value : def} }
 #define GGROUP(v, name, class) { AP_PARAM_GROUP, name, Parameters::k_param_ ## v, &plane.g.v, {group_info : class::var_info} }
+#define GSCALAR_SETHOOKS(v, name, def) { plane.g.v.vtype, name, Parameters::k_param_ ## v, &plane.g.v, {def_value : def}, AP_PARAM_FLAG_SETHOOKS }
 #define GOBJECT(v, name, class) { AP_PARAM_GROUP, name, Parameters::k_param_ ## v, (const void *)&plane.v, {group_info : class::var_info} }
 #define GOBJECTN(v, pname, name, class) { AP_PARAM_GROUP, name, Parameters::k_param_ ## pname, (const void *)&plane.v, {group_info : class::var_info} }
 
@@ -30,7 +31,7 @@ const AP_Param::Info Plane::var_info[] = {
     // @Description: The identifier of the ground station in the MAVLink protocol. Don't change this unless you also modify the ground station to match.
     // @Range: 1 255
     // @User: Advanced
-    GSCALAR(sysid_my_gcs,           "SYSID_MYGCS",    255),
+    GSCALAR_SETHOOKS(sysid_my_gcs,           "SYSID_MYGCS",    255),
 
     // @Group: SERIAL
     // @Path: ../libraries/AP_SerialManager/AP_SerialManager.cpp
