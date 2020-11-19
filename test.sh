15caf5fd2eab353ae7378039f5a8225dcac87a9f
Peter Barker <pbarker@barker.dropbear.id.au>
2020-11-19 14:36:29 +1100

    test.sh: add trivial test script

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# it is expected that you've done a "./waf configure --board=sitl --debug"
+# before running this script.
+
+IMU_DATA_FILE="./Tools/Replay/sample-input/2020_04_09_07_48_33_ADIS16465.csv"
+POS_DATA_FILE="./Tools/Replay/sample-input/2020_04_09_07_48_33_UBLOX.pos"
+
+reset
+./waf build --target=tools/Replay
+gdb --args ./build/sitl/tools/Replay $IMU_DATA_FILE $POS_DATA_FILE
