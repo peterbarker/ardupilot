f7d0b12628fb29056a5cc4aed46941f742df5a97
Peter Barker <pbarker@barker.dropbear.id.au>
2022-01-07 12:36:48 +1100

    SITL: new Loweheiser generator/efi driver and sim
    SITL: break out common GeneratorEngine code

--- libraries/SITL/SIM_RichenPower.h
+++ libraries/SITL/SIM_RichenPower.h
@@ -45,6 +45,7 @@ arm throttle (denied because generator not running)
 #include "SITL_Input.h"
 
 #include "SIM_SerialDevice.h"
+#include "SIM_GeneratorEngine.h"
 
 namespace SITL {
 
@@ -96,8 +97,6 @@ private:
 
     float _current_current;
 
-    uint32_t last_rpm_update_ms;
-
     // packet to send:
     struct PACKED RichenPacket {
         uint8_t magic1;
@@ -135,6 +134,8 @@ private:
 
     // time we were asked to stop; 
     uint32_t stop_start_ms;
+
+    SIM_GeneratorEngine generatorengine;
 };
 
 }
