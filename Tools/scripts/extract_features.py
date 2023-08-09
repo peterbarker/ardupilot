b54b70e00c85053a6391c94b3713c36ea0ba42e6
Shiv Tyagi <shivtyagi3015@gmail.com>
2023-08-09 17:07:55 +0530

    Tools: add AP_LocationDB in build_options.py
    Tools: add entry for AP_LocationDB in extract_features.py

--- Tools/scripts/extract_features.py
+++ Tools/scripts/extract_features.py
@@ -285,6 +285,7 @@ class ExtractFeatures(object):
             ('AP_QUICKTUNE_ENABLED', r'AP_Quicktune::update'),
             ('AP_FILTER_ENABLED', r'AP_Filters::update'),
             ('AP_CAN_LOGGING_ENABLED', r'AP_CANManager::can_logging_callback'),
+            ('AP_LOCATIONDB_ENABLED', r'AP_LocationDB::init',),
         ]
 
     def progress(self, msg):
