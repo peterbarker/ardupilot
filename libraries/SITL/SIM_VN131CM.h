5016593d99c8357928636082b0b78e20ad6971c7
Peter Barker <pbarker@barker.dropbear.id.au>
2023-12-06 12:06:39 +1100

    SITL: correct pressure values in packet, add scaling based on control reg

--- libraries/SITL/SIM_VN131CM.h
+++ libraries/SITL/SIM_VN131CM.h
@@ -18,7 +18,9 @@ namespace SITL {
 class VN131CM : public I2CDevice
 {
 public:
-    void init() override { }
+    void init() override {
+        reset();
+    }
 
     void update(const class Aircraft &aircraft) override;
 
@@ -26,13 +28,23 @@ public:
 
 private:
 
+    void reset() {
+        reg_mode = 0x3f;
+        reg_rate_control = 0x01;
+    }
+
     int rd(I2C::i2c_rdwr_ioctl_data *&data);
     int wr(I2C::i2c_rdwr_ioctl_data *&data);
 
-
     void get_pressure_temperature_readings(float &P_Pa, float &Temp_C);
+    uint32_t pressure_field_value(float press) const;
 
-    uint8_t mode;
+    uint8_t reg_mode;
+    uint8_t reg_rate_control;
+
+    uint8_t new_reg_mode;
+    uint8_t new_reg_rate_control;
+    bool one_cycle_latency;
 };
 
 } // namespace SITL
