f0860053d4afbe7405ac483542ce442abb0b1d92
Peter Barker <pbarker@barker.dropbear.id.au>
2023-12-06 12:06:39 +1100

    SITL: correct pressure values in packet, add scaling based on control reg

--- libraries/SITL/SIM_VN131CM.cpp
+++ libraries/SITL/SIM_VN131CM.cpp
@@ -17,6 +17,7 @@ int VN131CM::rd(I2C::i2c_rdwr_ioctl_data *&data)
     float temperature_C;
     float pressure_Pa;
     get_pressure_temperature_readings(pressure_Pa, temperature_C);
+    const uint32_t pressure = pressure_field_value(pressure_Pa);
 
     switch (msg.len) {
     case 24:        // build number, 6 bytes ascii, null-terminated
@@ -32,19 +33,25 @@ int VN131CM::rd(I2C::i2c_rdwr_ioctl_data *&data)
         strncpy((char*)&msg.buf[6], "VN131CM", 8);
         FALLTHROUGH;
     case 6:         // temperature, 2-byte sign int
-        msg.buf[5] = (temperature_C-int(temperature_C))*255;
+        msg.buf[5] = (abs(temperature_C) - int(abs(temperature_C))) * 256;
         msg.buf[4] = temperature_C;
         FALLTHROUGH;
     case 4:         // pressure, 3-byte unsigned int
-        msg.buf[3] = 17;
-        msg.buf[2] = 18;
-        msg.buf[1] = 19;
+        msg.buf[3] = pressure;
+        msg.buf[2] = pressure >> 8;
+        msg.buf[1] = pressure >> 16;
         FALLTHROUGH;
     case 1:         // mode
-        msg.buf[0] = mode;
+        msg.buf[0] = reg_mode;
         break;
     default:
         AP_HAL::panic("bad msg read len");
+    }
+
+    if (one_cycle_latency) {
+        reg_mode = new_reg_mode;
+        reg_rate_control = new_reg_rate_control;
+        one_cycle_latency = false;
     }
 
     return 0;
@@ -60,12 +67,17 @@ int VN131CM::wr(I2C::i2c_rdwr_ioctl_data *&data)
 
     switch (msg.len) {
     case 2:
+        // setting both control and rate registers
+        new_reg_rate_control = msg.buf[1];
         FALLTHROUGH;
     case 1:
+        new_reg_mode = msg.buf[0];
         break;
     default:
         AP_HAL::panic("Bad msg write len");
     }
+
+    one_cycle_latency = true;
 
     return 0;
 }
@@ -81,6 +93,22 @@ int VN131CM::rdwr(I2C::i2c_rdwr_ioctl_data *&data)
 
 void VN131CM::update(const class Aircraft &aircraft)
 {
+}
+
+// returns the three-bytes required for a pressure reading:
+uint32_t VN131CM::pressure_field_value(float press) const
+{
+    const float vn131cm_range[8] = {23.6, 27.5, 31.5, 35.4, 39.4, 43.3, 47.2, 51.2}; //all in inH2O
+    const float _current_range_val = vn131cm_range[reg_mode & 0x7];
+
+    const float inH20_to_Pa = 249.08f;
+
+    float diff_press_inH2O = press / inH20_to_Pa;
+
+    // const float margin = 29491.2f;
+
+    const uint32_t dp_raw = ((diff_press_inH2O * 15099493.5f) / _current_range_val) + 8388607.5f;
+    return dp_raw;
 }
 
 // FIXME: this was copied in from MS5525
