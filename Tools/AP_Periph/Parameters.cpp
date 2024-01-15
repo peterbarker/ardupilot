MonashUAS/ardupilot_esc_telem

AP_Periph: support AP_ADSB library in AP_Periph
sources information from a DroneCAN GPS on the bus to fill in data for the AP_ADSB library to consume and send to AP_ADSB modules

Tools/AP_Periph/Parameters.cpp
@@ -329,6 +329,12 @@ const AP_Param::Info AP_Periph_FW::var_info[] = {
     GSCALAR(adsb_port, "ADSB_PORT", HAL_PERIPH_ADSB_PORT_DEFAULT),
 #endif
 
+#ifdef AP_PERIPH_ENABLE_ADSB_OUT
+    // @Group: ADSB_
+    // @Path: ../libraries/AP_ADSB/AP_ADSB.cpp
+    GOBJECT(adsb_lib, "ADSB_", AP_ADSB),
+#endif
+
 #ifdef HAL_PERIPH_ENABLE_PWM_HARDPOINT
     // @Param: HARDPOINT_ID
     // @DisplayName: Hardpoint ID

peterbarker/ardupilot

Tools/AP_Periph/Parameters.cpp
@@ -351,6 +351,12 @@ const AP_Param::Info AP_Periph_FW::var_info[] = {
     GSCALAR(adsb_port, "ADSB_PORT", HAL_PERIPH_ADSB_PORT_DEFAULT),
 #endif
 
+#ifdef HAL_PERIPH_ENABLE_ADSB_LIBRARY
+    // @Group: ADSB_
+    // @Path: ../libraries/AP_ADSB/AP_ADSB.cpp
+    GOBJECT(adsb_library_adsb, "ADSB_", AP_ADSB),
+#endif
+
 #ifdef HAL_PERIPH_ENABLE_PWM_HARDPOINT
     // @Param: HARDPOINT_ID
     // @DisplayName: Hardpoint ID