3e881f81989b576c4038674f45b0ed61c2ae92b5
Leonard Hall <leonardthall@gmail.com>
2024-03-02 12:27:36 +1030

    Copter: Payload Place: Change PLDP_RNG_MIN to PLDP_RNG_MAX

--- ArduCopter/mode_auto.cpp
+++ ArduCopter/mode_auto.cpp
@@ -1363,7 +1363,7 @@ void PayloadPlace::run_vertical_control()
                 state = State::Ascent_Start;
                 gcs().send_text(MAV_SEVERITY_WARNING, "%s PLDP_RNG_MIN set and rangefinder not enabled", prefix_str);
                 break;
-            } else if (copter.rangefinder_alt_ok() && (copter.rangefinder_state.glitch_count == 0) && (copter.rangefinder_state.alt_cm > g2.pldp_range_finder_minimum_m * 100.0)) {
+            } else if (copter.rangefinder_alt_ok() && (copter.rangefinder_state.glitch_count == 0) && (copter.rangefinder_state.alt_cm > g2.pldp_range_finder_maximum_m * 100.0)) {
                 // range finder altitude is above minimum
                 place_start_time_ms = now_ms;
                 break;
