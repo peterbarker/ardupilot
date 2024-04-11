af7622eecb3762914d7a8acb1715ff5f18b9ec7a
Peter Barker <pbarker@barker.dropbear.id.au>
2024-04-11 19:14:57 +1000

    AP_Follow: use Location.chnage_alt_frame for what it is good for

--- libraries/AP_Follow/AP_Follow.cpp
+++ libraries/AP_Follow/AP_Follow.cpp
@@ -216,7 +216,7 @@ bool AP_Follow::get_target_dist_and_vel_ned(Vector3f &dist_ned, Vector3f &dist_w
 
     // change to altitude above home if relative altitude is being used
     if (target_loc.relative_alt == 1) {
-        current_loc.alt -= AP::ahrs().get_home().alt;
+        current_loc.change_alt_frame(Location::AltFrame::ABOVE_HOME);
     }
 
     // calculate difference
