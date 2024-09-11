f56617929a1e70e39c42b7cdc3680f2b441f56b2
Andrew Tridgell <andrew@tridgell.net>
2024-09-11 14:27:25 +1000

    Tools: added feature selection for CINS

--- Tools/scripts/extract_features.py
+++ Tools/scripts/extract_features.py
@@ -68,6 +68,7 @@ class ExtractFeatures(BuildScriptBase):
             ('HAL_NAVEKF3_AVAILABLE', 'NavEKF3::NavEKF3',),
             ('HAL_NAVEKF2_AVAILABLE', 'NavEKF2::NavEKF2',),
             ('AP_EXTERNAL_AHRS_ENABLED', r'AP_ExternalAHRS::init\b',),
+            ('AP_AHRS_CINS_ENABLED', 'CINS::init',),
             ('AP_EXTERNAL_AHRS_{type}_ENABLED', r'AP_ExternalAHRS_(?P<type>.*)::healthy\b',),
             ('HAL_INS_TEMPERATURE_CAL_ENABLE', 'AP_InertialSensor_TCal::Learn::save_calibration',),
             ('HAL_VISUALODOM_ENABLED', 'AP_VisualOdom::init',),
