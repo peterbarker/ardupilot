2260d031a5523995757d69e3416115075411d1d4
Peter Barker <pbarker@barker.dropbear.id.au>
2024-09-20 22:18:38 +1000

    AP_GPS: move initialisation of some members into header

--- libraries/AP_GPS/AP_GPS_UBLOX.cpp
+++ libraries/AP_GPS/AP_GPS_UBLOX.cpp
@@ -96,12 +96,6 @@ extern const AP_HAL::HAL& hal;
 
 AP_GPS_UBLOX::AP_GPS_UBLOX(AP_GPS &_gps, AP_GPS::GPS_State &_state, AP_HAL::UARTDriver *_port, AP_GPS::GPS_Role _role) :
     AP_GPS_Backend(_gps, _state, _port),
-    _next_message(STEP_PVT),
-    _ublox_port(255),
-    _unconfigured_messages(CONFIG_ALL),
-    _hardware_generation(UBLOX_UNKNOWN_HARDWARE_GENERATION),
-    next_fix(AP_GPS::NO_FIX),
-    noReceivedHdop(true),
     role(_role)
 {
     // stop any config strings that are pending
