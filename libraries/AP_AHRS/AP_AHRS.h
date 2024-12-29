06241b943a2d8115ea734a4ab01bbd04b539fdd0
Peter Barker <pbarker@barker.dropbear.id.au>
2024-12-30 02:19:59 +1100

    AP_AHRS: use active_backend for get_hgt_control_limit

--- libraries/AP_AHRS/AP_AHRS.h
+++ libraries/AP_AHRS/AP_AHRS.h
@@ -383,7 +383,9 @@ public:
     // get_hgt_ctrl_limit - get maximum height to be observed by the control loops in meters and a validity flag
     // this is used to limit height during optical flow navigation
     // it will return invalid when no limiting is required
-    bool get_hgt_ctrl_limit(float &limit) const;
+    bool get_hgt_ctrl_limit(float &limit) const {
+        return active_backend->get_hgt_ctrl_limit(limit);
+    }
 
     // Set to true if the terrain underneath is stable enough to be used as a height reference
     // this is not related to terrain following
