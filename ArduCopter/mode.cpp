b3a4b4760235d19789a85aee85ea117da4cc29e9
Iampete1 <iampete@hotmail.co.uk>
2025-07-16 02:16:29 +0100

    Copter: `WP_NAVALT_MIN` only apply to takeoff

--- ArduCopter/mode.cpp
+++ ArduCopter/mode.cpp
@@ -778,27 +778,6 @@ void Mode::land_run_horizontal_control()
     pos_control->update_xy_controller();
     Vector3f thrust_vector = pos_control->get_thrust_vector();
 
-    if (g2.wp_navalt_min > 0) {
-        // user has requested an altitude below which navigation
-        // attitude is limited. This is used to prevent commanded roll
-        // over on landing, which particularly affects helicopters if
-        // there is any position estimate drift after touchdown. We
-        // limit attitude to 7 degrees below this limit and linearly
-        // interpolate for 1m above that
-        const float attitude_limit_cd = linear_interpolate(700, copter.aparm.angle_max, get_alt_above_ground_cm(),
-                                                     g2.wp_navalt_min*100U, (g2.wp_navalt_min+1)*100U);
-        const float thrust_vector_max = sinf(radians(attitude_limit_cd * 0.01f)) * GRAVITY_MSS * 100.0f;
-        const float thrust_vector_mag = thrust_vector.xy().length();
-        if (thrust_vector_mag > thrust_vector_max) {
-            float ratio = thrust_vector_max / thrust_vector_mag;
-            thrust_vector.x *= ratio;
-            thrust_vector.y *= ratio;
-
-            // tell position controller we are applying an external limit
-            pos_control->set_externally_limited_xy();
-        }
-    }
-
     // call attitude controller
     attitude_control->input_thrust_vector_heading(thrust_vector, auto_yaw.get_heading());
 
