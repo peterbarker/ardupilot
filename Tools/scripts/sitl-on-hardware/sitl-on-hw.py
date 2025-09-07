218ed2ad5b93702295da80ad0cf06c2efaa0a5fc
Peter Barker <pbarker@barker.dropbear.id.au>
2025-09-07 11:46:54 +0100

    Tools: add ability for sim-on-hardware to use Simulink

--- Tools/scripts/sitl-on-hardware/sitl-on-hw.py
+++ Tools/scripts/sitl-on-hardware/sitl-on-hw.py
@@ -17,6 +17,8 @@ parser.add_argument("--frame", default=None, help="frame type")
 parser.add_argument("--simclass", default=None, help="simulation class")
 parser.add_argument("--defaults", default=None, help="extra defaults file")
 parser.add_argument("--upload", action='store_true', default=False, help="upload firmware")
+parser.add_argument("--debug", action='store_true', default=False, help="create debug build")
+parser.add_argument("--extra-hwdef", action='append', default=[], help="extra hwdef files")
 
 args, unknown_args = parser.parse_known_args()
 
@@ -27,14 +29,18 @@ def run_program(cmd_list):
     print("Running (%s)" % " ".join(cmd_list))
     retcode = subprocess.call(cmd_list)
     if retcode != 0:
-       print("FAILED: %s" % (' '.join(cmd_list)))
-       global extra_hwdef
-       if extra_hwdef is not None:
-           extra_hwdef.close()
-           os.unlink(extra_hwdef.name)
-       sys.exit(1)
+        print("FAILED: %s" % (' '.join(cmd_list)))
+        sys.exit(1)
 
-extra_hwdef = tempfile.NamedTemporaryFile(mode='w')
+
+frame_options = sorted(vinfo.options[vehicle_map[args.vehicle]]["frames"].keys())
+frame_options_string = ' '.join(frame_options)
+if args.frame and args.frame not in frame_options and not not args.simclass.startswith('json:'):
+    print(f"ERROR: invalid frame {args.frame}; must be one of {frame_options_string}")
+    sys.exit(1)
+
+
+extra_hwdef = tempfile.NamedTemporaryFile(mode='w', delete=False)
 extra_defaults = tempfile.NamedTemporaryFile(mode='w')
 
 def hwdef_write(s):
@@ -59,6 +65,9 @@ else:
 # add base hwdef to extra_hwdef
 hwdef_write(open(sohw_path(extra_hwdef_base), "r").read() + "\n")
 
+for f in args.extra_hwdef:
+    hwdef_write(open(f, "r").read() + "\n")
+
 # add base defaults to extra_defaults
 defaults_write(open(sohw_path(defaults_base), "r").read() + "\n")
 
@@ -66,6 +75,8 @@ if args.defaults:
     defaults_write(open(args.defaults,"r").read() + "\n")
 
 if args.simclass:
+    if args.simclass == 'JSON':
+        hwdef_write("define AP_SIM_JSON_ENABLED 1\n")
     hwdef_write("define AP_SIM_FRAME_CLASS %s\n" % args.simclass)
 if args.frame:
     hwdef_write('define AP_SIM_FRAME_STRING "%s"\n' % args.frame)
